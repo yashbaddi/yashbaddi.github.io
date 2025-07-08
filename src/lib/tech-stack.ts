e7d236e602291463336711a21c1b54539c633ed5
Yash Baddi <yashbaddi29@gmail.com>
2025-07-08 17:29:38 +0530

    Add tech stacks

---
+++ src/lib/tech-stack.ts
@@ -0,0 +1,98 @@
+import {
+  AWS,
+  Docker,
+  Figma,
+  Git,
+  MySQL,
+  NodeJs,
+  PostgreSQL,
+  Python,
+  React,
+  Redis,
+  RemixDarkWordmark,
+  ShadcnUI,
+  TailwindCSS,
+  TypeScript,
+} from "developer-icons";
+
+export const TECH_STACK = [
+  // Programming Languages
+  {
+    title: "TypeScript",
+    href: "https://www.typescriptlang.org/",
+    icon: TypeScript,
+  },
+  {
+    title: "Python",
+    href: "https://www.python.org/",
+    icon: Python,
+  },
+  // Backend Technologies
+  {
+    title: "Node.js",
+    href: "https://nodejs.org/",
+    icon: NodeJs,
+  },
+
+  // Databases & Caching
+  {
+    title: "PostgreSQL",
+    href: "https://www.postgresql.org/",
+    icon: PostgreSQL,
+  },
+  {
+    title: "MySQL",
+    href: "https://www.mysql.com/",
+    icon: MySQL,
+  },
+  {
+    title: "Redis",
+    href: "https://redis.io/",
+    icon: Redis,
+  },
+
+  // DevOps & Development Tools
+  {
+    title: "Docker",
+    href: "https://www.docker.com/",
+    icon: Docker,
+  },
+  {
+    title: "Git",
+    href: "https://git-scm.com/",
+    icon: Git,
+  },
+  {
+    title: "AWS",
+    href: "https://aws.amazon.com/",
+    icon: AWS,
+  },
+  // Frontend Technologies
+  {
+    title: "React",
+    href: "https://react.dev/",
+    icon: React,
+  },
+  {
+    title: "Remix.js",
+    href: "https://remix.run/",
+    icon: RemixDarkWordmark,
+  },
+  {
+    title: "Tailwind CSS",
+    href: "https://tailwindcss.com/",
+    icon: TailwindCSS,
+  },
+  {
+    title: "shadcn/ui",
+    href: "https://ui.shadcn.com/",
+    icon: ShadcnUI,
+  },
+
+  // Miscellaneous
+  {
+    title: "Figma",
+    href: "https://www.figma.com/",
+    icon: Figma,
+  },
+];
